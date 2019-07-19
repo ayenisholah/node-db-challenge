ded67627ea36145958cd0236e17346aaf63cd951
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-19 09:10:26 +0100

    Endpoint to add project actions

--- projects/project-router.js
+++ projects/project-router.js
@@ -15,3 +15,20 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/:id/actions', async (req, res) => {
+  const actionData = req.body;
+  const { id } = req.params;
+
+  try {
+    const project = await getByID(id);
+
+    if (project) {
+      const action = await Projects.addActions(actionData, id);
+      res.status(201).json(action);
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to create new action' });
+  }
+});
