9da684ede87623eb1371df110ec7e4166da11c55
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-19 08:27:16 +0100

    Actions Table Schema

--- data/migrations/20190719081202_project.js
+++ data/migrations/20190719081202_project.js
@@ -3,10 +3,21 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('projects', table => {
       table.increments();
-      table.text('project_name')
+      table.text('project_name', 50)
         .unique()
         .notNullable();
       table.text('description', 128)
+        .notNullable();
+      table.boolean('completed')
+        .notNullable();
+    })
+
+    .createTable('actions', table => {
+      table.increments();
+      table.text('action_name', 50)
+        .unique()
+        .notNullable();
+      table.text('notes', 128)
         .notNullable();
       table.boolean('completed')
         .notNullable();
